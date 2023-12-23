a9b1e09ac2088df08b742f2269899f364da5b0fa
ntonnes <noah.tonnesen@gmail.com>
2023-12-23 16:37:18 -0500

    Last commit before changing piece structure

--- lib/draw.ml
+++ lib/draw.ml
@@ -23,27 +23,48 @@ let render_chessboard renderer =
     for col = 0 to 7 do
 
       (* draw board square *)
-      let rect = Sdl.Rect.create ~x:(col * cell_size) ~y:(row * cell_size) ~w:cell_size ~h:cell_size in
+      let rect = Sdl.Rect.create 
+        ~x:((col * !cell_size) + !offset_x) 
+        ~y:((row * !cell_size) + !offset_y) 
+        ~w:!cell_size 
+        ~h:!cell_size 
+      in
+
       if (row + col) mod 2 = 0 then
         Sdl.set_render_draw_color renderer 255 206 158 255 |> ignore
       else
         Sdl.set_render_draw_color renderer 209 139 71 255 |> ignore;
       Sdl.render_fill_rect renderer (Some rect) |> ignore;
+      
     done
-  done;
-
-  Sdl.render_present renderer;
+  done
 ;;
 
 
-let update renderer game_state =
+let render_pieces renderer game_state =
   for row = 0 to 7 do
     for col = 0 to 7 do
 
       (* render piece within cell *)
-      let rect = Sdl.Rect.create ~x:((col * cell_size)+10) ~y:((row * cell_size)+10) ~w:(cell_size-20) ~h:(cell_size-20) in
+      let rect = Sdl.Rect.create 
+        ~x:((col * !cell_size)+10+ !offset_x) 
+        ~y:((row * !cell_size)+10+ !offset_y) 
+        ~w:(!cell_size-20) 
+        ~h:(!cell_size-20) 
+      in
+
       let cell = (game_state).(row).(col) in
       if get_piece renderer cell then Sdl.render_fill_rect renderer (Some rect) |> ignore
     
     done
   done
+;;
+
+let refresh_window window renderer game_state =
+
+  let (w, h) = (!window_width, !window_height) in
+  if Sdl.get_window_size window <> (w, h) then update_constants window; render_chessboard renderer;
+  render_pieces renderer game_state;
+  Sdl.render_present renderer;
+;;
+
